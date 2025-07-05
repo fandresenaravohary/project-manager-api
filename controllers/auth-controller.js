48cb4db050e15ec336891f1496678789db604565
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-07-05 15:19:27 +0300

    feat: implement registerUser method

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,45 @@
-const registerUser = async (req, res) => {}
-const loginUser = async (req, res) => {}
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+const registerUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email address already in use" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    // TODO: send email
+
+    res.status(201).json({
+      message:
+        "Verification email sent to your mail. Please check and verify your account.",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export { registerUser, loginUser };
