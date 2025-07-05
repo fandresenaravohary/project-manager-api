f972fc5e27c1e70bac21088ae1baceca002398c9
Fandresena Ravohary <hei.fandresena@gmail.com>
2025-07-05 10:24:31 +0300

    feat: DB connection

---
+++ index.js
@@ -0,0 +1,47 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(morgan("dev"));
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("Failed to connect to DB", err));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Welcome to TaskHub API" });
+});
+
+// error middleware
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+// not found middleware
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
